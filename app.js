2250160717b9c37f32fb1c6f567d763f2bbbf9e7
RedFennecJerre1 <133092194+RedFennecJerre1@users.noreply.github.com>
2024-01-14 15:07:26 +0100

    update and delete people and posts

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const { urlencoded } = require('body-parser');
 const app = express();
+const Person = require('./models/personSchema');
+const Post = require('./models/postSchema');
 app.use(urlencoded({ extended: true }));
 const port = 3000;
+
+
+const personRoutes = require('./routes/personRoutes');
+app.use('/person', personRoutes);
+
+const postRoutes = require('./routes/postRoutes');
+app.use('/post', postRoutes);
+
 
 app.set('view engine', 'ejs');
 
@@ -10,16 +20,86 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/person/add', (req, res) => {
-    res.render('add-person');
-});
+
+
+app.get('/people', async (req, res) => {
+    // Fetch all people from the database
+    const people = await Person.find({});
+  
+    // Render the view with the list of people
+    res.render('person', { people });
+  });
+
+// Route for updating a person
+app.get("/people/:id", async (req, res) => {
+    const person = await Person.findById(req.params.id);
+    res.render("peopleEdit", { person });
+  });
+  
+app.post("/people/:id", async (req, res) => {
+    const updatedPerson = await Person.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+    res.redirect("/people");
+  });
+
+  app.post("/people/delete/:id", async (req, res) => {
+    try {
+        const person = await Person.findByIdAndDelete(req.params.id);
+  
+        if (!person) {
+          return res.status(404).send();
+        }
+  
+        res.redirect("/people");
+      } catch (error) {
+        res.status(500).send(error);
+      }
+  });
+
+
+  app.get('/posts', async (req, res) => {
+    const post = await Post.find({});
+    res.render('post', { post });
+  });
+
+
+app.get("/posts/:id", async (req, res) => {
+    const post = await Post.findById(req.params.id);
+    res.render("postEdit", { post });
+  });
+  
+app.post("/posts/:id", async (req, res) => {
+    const updatedPost = await Post.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+    res.redirect("/posts");
+  });
+
+  app.post("/posts/delete/:id", async (req, res) => {
+    try {
+        const post = await Post.findByIdAndDelete(req.params.id);
+  
+        if (!post) {
+          return res.status(404).send();
+        }
+  
+        res.redirect("/posts");
+      } catch (error) {
+        res.status(500).send(error);
+      }
+  });
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-const personRoutes = require('./routes/personRoutes');
-app.use('/person', personRoutes);
+
 
 
 //mongoDB
---
+++ routes/postRoutes.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/postSchema');
+
+// Create a new post
+router.post('/', async (req, res) => {
+  const post = new Post(req.body);
+
+  try {
+    await post.save();
+    res.redirect("/posts");
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Retrieve all posts
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.render('post', { post });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.get('/add', (req, res) => {
+    res.render('add-post');
+});
+
+// Retrieve a single post with a given id
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).send();
+    }
+
+    res.send(post);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Update a post with a given id
+router.patch('/:id', async (req, res) => {
+  try {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+    if (!post) {
+      return res.status(404).send();
+    }
+
+    res.send(post);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Delete a post with a given id
+router.delete('/:id', async (req, res) => {
+  try {
+    const post = await Post.findByIdAndDelete(req.params.id);
+
+    if (!post) {
+      return res.status(404).send();
+    }
+
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
